e68ecc98aff49bc45366377322834c49ca7bb61d
Luís Alves <luisalvesw@gmail.com>
2021-06-20 18:07:13 +0100

    Generic command

--- bot.py
+++ bot.py
@@ -16,21 +16,11 @@ async def on_ready():
 @client.event
 async def on_message(message):
     
-    if message.content.startswith('>hello') or message.content.startswith('>hello'):
-        await message.channel.send('Heyo! Para começar o servidor usem $start_minecraft')
-
-    if message.content.startswith('$start_minecraft'):
+    if message.content.startswith('$'):
         payload = json.dumps({
-        "command": "start"
+        "command": message.content
         })
         response = requests.request("POST", url, headers=headers, data=payload)
         await message.channel.send('Server is starting! Wait 30s...')
-
-    if message.content.startswith('$stop_minecraft') and message.content.endswith(os.getenv('STOP_KEYWORD')):
-        payload = json.dumps({
-        "command": "stop"
-        })
-        response = requests.request("POST", url, headers=headers, data=payload)
-        await message.channel.send('Server is stopping...')
 
 client.run(os.getenv("DISCORD_TOKEN"))
